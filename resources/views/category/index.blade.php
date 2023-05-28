0b17422da19416b7a9eba7797bc71213066fe242
DeaNurita <deanuritafebriana@gmail.com>
2023-05-28 10:37:35 +0700

    [feat] t24 membuat create pada kategori

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -4,6 +4,9 @@
 <main>
     <div class="container-fluid px-4">
         <h1 class="mt-4">Category</h1>
+
+        <a class="btn btn-primary mb-2" href="{{ route('category.create') }}" role="button">Create New</a>
+
         <div class="card mb-4">
             <div class="card-body">
                 <table id="datatablesSimple">
